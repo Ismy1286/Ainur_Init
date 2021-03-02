1a89bc8fca2420a01384c1d5614a6c9f2698d223
user <user@MacBook-Air-user.local>
2021-03-02 12:12:36 +0600

    updated 2marta

---
+++ Lesson6.py
@@ -0,0 +1,97 @@
+# isklucheniya---------
+
+# deleniye na nol  100 / 100
+
+# slojeniye str i int  (1+'23')
+
+# try:
+#     print(1 / 0)
+# except ZeroDivisionError:
+#     print('Ошибка деления на ноль')
+# print('Код далше работвет')
+#
+# try:
+#     print(1 + '1')
+# except Exception as ex:
+#     print(ex)
+# print('Код дальше работает')
+
+
+#
+# try:
+#     print(1 + 1)
+# except Exception as ex:
+#     print(ex)
+# finally:
+#     print('Эта строка кода работает в любом случае')
+# print('Код дальше работает')
+
+
+# raise NameError('Hello')
+#
+# print(num)
+#
+# for i in range(20):
+#    print(i) (3-пробела)
+#
+# lesson3 = open('lesson3.py', 'r')
+# for line in lesson3:
+#     print(line)
+
+# with open("test.txt", 'w') as file:
+#     file.write("lesson3 = open('lesson3.py', 'r')")
+#     file.write('Эта строка работает в любом случае \n')
+#
+# with open("test.txt", 'a') as file:
+#     file.write('new data')
+#
+
+# color = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+# with open("text.txt", 'w') as file:
+#     for i in color:
+#         file.write(i +'\n')
+#
+# with open('text.txt') as file:
+#     string_of_file = file.read()
+#     print(strings_of_file)
+
+
+# print(ord('a'))
+
+# import os (operasionka---
+# os.remove("text.txt")
+
+# import shutil
+# shutil.copyfile('lesson1.py', 'lesn1.py')
+
+#
+# import random
+# import time
+# random_list = [random.randint(1, 1000) for i in range(1000) ]
+# random_list.sort()
+# random.shuffle(random_list)
+# print(random_list)
+
+# list = [i for i in range(50)]
+# for i in range(20)
+#     print(random.choice(list))
+#     time.asleep(random.randrange(1, 5))
+
+# SORTIROVKA SLIYALIY----UPORYADOCHINEYE SPISKAN
+#
+# list_ = [33, 5, 14, 89, 22, 41]
+# sorted_list = sorted(list_)
+# # list_.sort()
+#
+# print(sorted_list)
+
+
+
+#
+#
+# import os
+# os.remove("lesn1.py")
+
+#
+# import shutil
+# shutil.copyfile('chapter_1_1.py', 'chapter1_2.py')
